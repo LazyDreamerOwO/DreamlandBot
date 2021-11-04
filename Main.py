d2fa83a2672c511d93574452bcdb4eaebca80bc1
TheGuy566 <54692320+TheGuy566@users.noreply.github.com>
2021-11-04 13:25:50 -0400

    Converted from 'client' to 'bot'
    Changed based on recommendations here: https://stackoverflow.com/questions/51234778/what-are-the-differences-between-bot-and-client

--- Main.py
+++ Main.py
@@ -1,25 +1,28 @@
-import discord
 import os
 import random
 from dotenv import load_dotenv
+from discord.ext import commands
+
+
 load_dotenv(".env")
 
-client = discord.Client()
+# client = discord.Client()
+bot = commands.Bot(command_prefix='!')
 
-@client.event
+@bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(bot))
 
-@client.event
+@bot.event
 async def on_message(message):
     username = str(message.author).split('#')[0]
     user_message = str(message.content)
     channel = str(message.channel.name)
     print(f'{username}: {user_message}: ({channel})')
 
-    if message.author == client.user:
+    if message.author == bot.user:
         return
-    
+
     if message.channel.name == 'bot-test':
         if user_message == 'hello':
             await message.channel.send(f'Hello {username}!')
@@ -28,4 +31,9 @@ async def on_message(message):
             await message.channel.send(f'Goodbye {username}!')
             return
 
-client.run(os.environ.get("DiscordBotToken"))
+@bot.command()
+async def dice(sides : int):
+    """Adds two numbers together."""
+    await bot.say(f"You rolled a {random.randint(1, sides)} on a {sides} sided dice!")
+
+bot.run(os.environ.get("DiscordBotToken"))
