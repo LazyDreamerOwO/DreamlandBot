4d67572d813bccd57361fefaa65e94066bbab6e0
LazyDreamer <lazydreamerowo@gmail.com>
2021-11-04 16:27:53 +0000

    Added 'Hello' and 'Bye' Commands

--- Main.py
+++ Main.py
@@ -10,4 +10,22 @@ client = discord.Client()
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-client.run(os.environ.get("TOKEN"))
+@client.event
+async def on_message(message):
+    username = str(message.author).split('#')[0]
+    user_message = str(message.content)
+    channel = str(message.channel.name)
+    print(f'{username}: {user_message}: ({channel})')
+
+    if message.author == client.user:
+        return
+    
+    if message.channel.name == 'bot-test':
+        if user_message == 'hello':
+            await message.channel.send(f'Hello {username}!')
+            return
+        elif user_message == 'bye':
+            await message.channel.send(f'Goodbye {username}!')
+            return
+
+client.run(os.environ.get("DiscordBotToken"))
