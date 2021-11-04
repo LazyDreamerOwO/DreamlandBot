cbd5a7693d0de01bfbded0fa09fb661304cba8e0
veclav talica <hectorgreat19@gmail.com>
2021-11-05 00:03:44 +0500

    now works

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 
 load_dotenv(".env")
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='!')
 
 @bot.event
 async def on_ready():
@@ -27,11 +27,10 @@ async def on_message(message):
         elif user_message == 'bye':
             await message.channel.send(f'Goodbye {username}!')
             return
+        else:
+            await bot.process_commands(message)
 
-    else:
-        await bot.process_commands(message)
-
-@bot.command(name="dice")
+@bot.command()
 async def dice(ctx, sides: int):
     """Roll single dice with N sides"""
     await ctx.send(f"You rolled a {random.randint(1, sides)} on a {sides} sided dice!")
